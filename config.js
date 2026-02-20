71d0fc4468455f0ae9d4cef012faed419dc83dee
lookbag <toomanyjoke@gmail.com>
2026-02-19 21:03:50 -0500

    cofig.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://inhyqhtrpsjpiyywtfhw.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluaHlxaHRycHNqcGl5eXd0Zmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MjY5NTksImV4cCI6MjA4NzEwMjk1OX0.fL98cBPGk0itA3x8gWGTXt-5UIpYDXIMvplriZaTwKg'
+};
